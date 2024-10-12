47c9c2da9a9b311267195b8473a2098ee1e32cfa
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2024-10-12 18:43:38 +0500

    fully responsive

--- src/components/ActionCard.jsx
+++ src/components/ActionCard.jsx
@@ -1,55 +1,15 @@
-import {
-  Image,
-  Linking,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Card from './Card';
 
 export default function ActionCard() {
-  const openWebsite = link => {
-    Linking.openURL(link);
-  };
-
   return (
     <View>
       <Text style={styles.heading}>Action Card</Text>
-      <View style={styles.card}>
-        <Image
-          source={{
-            uri: 'https://img.freepik.com/free-photo/portrait-young-student-education-day_23-2150980069.jpg',
-          }}
-          style={styles.cardImage}
-        />
-        <View style={styles.cardBody}>
-          <Text style={styles.cardTitle}>Animated Boy</Text>
-          <Text numberOfLines={3} style={styles.cardDescription}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-            magnam, iure eveniet harum doloribus laborum adipisci magnam, iure
-            adipisci magnam. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Modi illo quo dolores earum blanditiis, fugit cupiditate atque
-            quibusdam tenetur ipsa nemo sint laborum itaque quas, ratione nulla
-            aliquam minima sequi..
-          </Text>
-          <View style={styles.btnContainer}>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => openWebsite('https://www.google.com/')}
-              style={styles.btn}>
-              <Text style={styles.cardFooter}>Read more</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() =>
-                openWebsite('https://www.instagram.com/hafizusman619/')
-              }
-              style={styles.btn}>
-              <Text style={styles.cardFooter}>Follow me</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+      <View style={styles.cardBox}>
+        <Card />
+        <Card />
+        <Card />
       </View>
     </View>
   );
@@ -62,45 +22,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginHorizontal: 14,
   },
-  card: {
-    minHeight: 200,
-    backgroundColor: '#242424',
-    borderRadius: 8,
-    marginTop: 8,
-    marginHorizontal: 20,
-  },
-  cardImage: {
-    height: 200,
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
-  },
-  cardBody: {
-    padding: 10,
-  },
-  cardTitle: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  cardDescription: {
-    color: 'white',
-    fontSize: 16,
-  },
-  cardFooter: {
-    color: 'black',
-  },
-  btnContainer: {
-    flex: 1,
+  cardBox: {
     flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 8,
-  },
-  btn: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#f1f1f1',
-    padding: 5,
-    borderRadius: 3,
-    marginHorizontal: 3,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
   },
 });
