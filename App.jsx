414dc6ba5482f6ae7202ce1a62ee181817bb3690
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2024-10-14 08:52:17 +0500

    Drawer Screen

--- App.jsx
+++ App.jsx
@@ -1,71 +1,52 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Login from './src/screens/Login';
-import Home from './src/screens/Home';
-import About from './src/screens/About';
-import Header from './src/screens/Header';
-import {Alert, Button} from 'react-native';
-import Example from './src/screens/Example';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Text, View} from 'react-native';
+import {Button} from 'react-native';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const sayHello = () => Alert.alert('Hello, User');
-
-const HeaderTitle = () => (
-  <Button title="One" color="green" onPress={sayHello} />
+const Home = ({navigation}) => (
+  <View>
+    <Text>Home</Text>
+    <Button title="Open Draw" onPress={() => navigation.openDrawer()} />
+  </View>
 );
 
-const HeaderRight = () => <Example />;
+const Profile = ({navigation}) => (
+  <View>
+    <Text>Profile</Text>
+    <Button title="Open Draw" onPress={() => navigation.openDrawer()} />
+  </View>
+);
+
+const Settings = ({navigation}) => (
+  <View>
+    <Text>Settings</Text>
+    <Button title="Open Draw" onPress={() => navigation.openDrawer()} />
+  </View>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-      // screenOptions={{
-      //   headerStyle: {},
-      //   headerTitleStyle: {},
-      //   headerTintColor: {},
-      //   contentStyle: {},
-      // }}
-      >
-        <Stack.Screen
-          name="Header"
-          component={Header}
-          options={{
-            title: '',
-            headerTitle: HeaderTitle,
-            headerRight: HeaderRight,
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            title: 'Login Form',
-            headerStyle: {
-              backgroundColor: 'green',
-            },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontSize: 20,
-              color: 'white',
-            },
-            contentStyle: {
-              backgroundColor: 'plum',
-            },
-          }}
-        />
-        <Stack.Screen
-          name="About"
-          component={About}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: 'plum',
+            width: 240,
+          },
+          drawerLabelStyle: {
+            color: 'black',
+            fontSize: 18,
+          },
+          // headerStyle: {},
+          // headerTintColor: {},
+          // headerTitleAlign: {},
+        }}>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Profile" component={Profile} />
+        <Drawer.Screen name="Settings" component={Settings} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
